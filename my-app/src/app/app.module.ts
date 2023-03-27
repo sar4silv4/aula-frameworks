b467bd8391898c6c8adf1af8e1e2d4cd10ee9562
Luana050 <luanaribeirosilva076@gmail.com>
2023-03-27 15:49:05 -0400

    Merge branch 'master' of https://github.com/sar4silv4/aula-frameworks

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './componets/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
