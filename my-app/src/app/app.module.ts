83a0955f150e6ab3a0b5a53009dd148f2207c7c8
sar4silv4 <127432663+sar4silv4@users.noreply.github.com>
2023-04-06 14:54:23 -0400

    Add files via upload

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './componets/hero/hero.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
   isError = false;
+
+  isSucess = false;
+
+  user = ""
+  pwd= ""
+
+  login() {
+  
+    console.log("User = " + this.user + "Password = " + this.pwd )
+    if( this.user == "sar4silv4" && this.pwd == "1234") { 
+      this.isError = false; 
+      this.isSucess = true
+      console.log ("Login Aprovado")
+     } else { 
+      this.isError = true;
+      this.isSucess = false;
+     }
+  }
+
+ 
+  
 }
+
