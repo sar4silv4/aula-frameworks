b7d6c3c9522ecc23d25ae396f547dc1785b6e16d
sar4silv4 <127432663+sar4silv4@users.noreply.github.com>
2023-05-29 16:37:45 -0400

    Add files via upload

--- my-app/src/app/components/create-user/create-user.component.ts
+++ my-app/src/app/components/create-user/create-user.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class CreateUserComponent {
 
+  isCorrect = false;
+  isIncorrect = false;
+
+  pwd1 = ""
+  pwd2 = ""
+
+  create (){if(this.pwd1 == "1234" && this.pwd2 == "1234") {
+    this.isIncorrect = false;
+    this.isCorrect = true;
+    console.log ("Senha Aprovada")
+   } else {
+    this.isIncorrect = true;
+    this.isCorrect = false
+  }
+  }
+
 }
--- my-app/src/app/components/task/task.component.ts
+++ my-app/src/app/components/task/task.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-task',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent {
+
+  nome = new FormControl('')
+  descricao = new FormControl('')
+  responsavel = new FormControl('')
+  dt_inicio = new FormControl('')
+  dt_fim = new FormControl('')
+
+  cadastrar () {}
 
 }
